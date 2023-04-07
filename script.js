e49b11a7a021d78c3184e8cd4d946e71d3d69ec0
Abbas <tundeeey@gmail.com>
2023-04-07 12:16:07 +0100

    added the previous and next button with their styling using bootstrap and css, also created a cbtQustion array in the script

---
+++ script.js
@@ -0,0 +1,31 @@
+let showQuestions = document.getElementById("show-questions");
+let showOptions = document.getElementById("show-options");
+let showResults = document.getElementById("show-result");
+
+let arrayCbt = [
+    {
+        question: "Who formed the first political party in Nigeria?",
+        options: ["Olusegun Obasanjo", "Muhammadu Buhari", "Herbert Macauly", "Agbani Darego",],
+        answers: "Herbert Macauly"
+    },
+    {
+        question: "Who formed the first political party in Nigeria?",
+        options: ["Olusegun Obasanjo", "Muhammadu Buhari", "Herbert Macauly", "Agbani Darego",],
+        answers: "Herbert Macauly"
+    },
+    {
+        question: "Who formed the first political party in Nigeria?",
+        options: ["Olusegun Obasanjo", "Muhammadu Buhari", "Herbert Macauly", "Agbani Darego",],
+        answers: "Herbert Macauly"
+    },
+    {
+        question: "Who formed the first political party in Nigeria?",
+        options: ["Olusegun Obasanjo", "Muhammadu Buhari", "Herbert Macauly", "Agbani Darego",],
+        answers: "Herbert Macauly"
+    },
+    {
+        question: "Who formed the first political party in Nigeria?",
+        options: ["Olusegun Obasanjo", "Muhammadu Buhari", "Herbert Macauly", "Agbani Darego",],
+        answers: "Herbert Macauly"
+    }
+];
