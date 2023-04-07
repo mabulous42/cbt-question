8c3787fa84f1c2b8b0ccc3ba488d81bf9f3465a4
Abbas <tundeeey@gmail.com>
2023-04-07 13:10:42 +0100

    Was able to display a question with its respective options using map, will now work on putting the options in a radio button and also work on click the next and previous button to show next and previous questions respectively

--- script.js
+++ script.js
@@ -5,27 +5,39 @@ let showResults = document.getElementById("show-result");
 let arrayCbt = [
     {
         question: "Who formed the first political party in Nigeria?",
-        options: ["Olusegun Obasanjo", "Muhammadu Buhari", "Herbert Macauly", "Agbani Darego",],
+        options: ["Olusegun Obasanjo", "Muhammadu Buhari", "Herbert Macauly", "Agbani Darego"],
         answers: "Herbert Macauly"
     },
     {
-        question: "Who formed the first political party in Nigeria?",
-        options: ["Olusegun Obasanjo", "Muhammadu Buhari", "Herbert Macauly", "Agbani Darego",],
+        question: "What is the name of Machester United Coach?",
+        options: ["Eddie Howe", "Frank Lampard", "Mikel Arteta", "Erik Ten Hag"],
+        answers: "Erik Ten Hag"
+    },
+    {
+        question: "What is the first political party in Nigeria?",
+        options: ["APC", "PDP", "NNDP", "AD"],
         answers: "Herbert Macauly"
     },
     {
-        question: "Who formed the first political party in Nigeria?",
-        options: ["Olusegun Obasanjo", "Muhammadu Buhari", "Herbert Macauly", "Agbani Darego",],
-        answers: "Herbert Macauly"
+        question: "Who is the current president of Nigeria?",
+        options: ["Muhammadu Buhari", "Bukola Saraki", "Goodluck Jonathan", "Olusegun Obasanjo"],
+        answers: "Muhammadu Buhari"
     },
     {
-        question: "Who formed the first political party in Nigeria?",
-        options: ["Olusegun Obasanjo", "Muhammadu Buhari", "Herbert Macauly", "Agbani Darego",],
-        answers: "Herbert Macauly"
-    },
-    {
-        question: "Who formed the first political party in Nigeria?",
-        options: ["Olusegun Obasanjo", "Muhammadu Buhari", "Herbert Macauly", "Agbani Darego",],
-        answers: "Herbert Macauly"
+        question: "What does the eagle in the Nigeria coat of arm represent?",
+        options: ["Peace", "Terror", "Agriculture", "Strength"],
+        answers: "Strength"
     }
 ];
+
+function next() {
+    
+}
+
+arrayCbt.map((cbt, index) => {
+    console.log(cbt);
+        showQuestions.innerHTML = `        
+            <h1>${index+1+"."} ${cbt.question}</h1>
+            <h5>${cbt.options}</h5>        
+        `
+})
