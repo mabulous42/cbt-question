1bec604be6e08c4c8c28705b3a901dffa451d010
Abbas <tundeeey@gmail.com>
2023-04-10 12:58:08 +0100

    I was able to get the value of the user answer to keep r track of the result if it is right or wrong, then consoleLog the user score at the end of the test, still unable to save the user choice yet after moving to the next question or previous question respectively

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ let showResults = document.getElementById("show-result");
 let prevBtn = document.getElementById("prev-btn");
 let nextBtn = document.getElementById('next-btn');
 let submitBtn = document.getElementById('submit-btn');
+let myOptions = document.getElementById('my-options');
 
 submitBtn.style.display = 'none';
+
+// function show(){   
+//     selectedOption = document.querySelector('input[name = option]:checked').value;
+//     console.log(selectedOption);
+// }
 
 let arrayCbt = [
     {
@@ -21,7 +27,7 @@ let arrayCbt = [
     {
         question: "What is the first political party in Nigeria?",
         options: ["APC", "PDP", "NNDP", "AD"],
-        answers: "Herbert Macauly"
+        answers: "NNDP"
     },
     {
         question: "Who is the current president of Nigeria?",
@@ -35,52 +41,104 @@ let arrayCbt = [
     }
 ];
 let count = 0;
-let track = 0;
 
 function SortQuestions() {
 
-    showQuestions.innerHTML = `
-        <h1>${count + 1 + "."} ${arrayCbt[count].question}</h1>           
-        ` 
-        for (let index = 0; index < arrayCbt[count].options.length; index++) {
-            showOptions.innerHTML += `
-            <h5>
-            <input name="answer" type="radio">
-            <label for="answer">${arrayCbt[count].options[index]}</label>
-            </h5>
+    arrayCbt.forEach((quiz, index) => {
+        if (index === count) {
+            index = count;
+            showQuestions.innerHTML = `
+            <h1>${index + 1 + "."} ${quiz.question}</h1>           
             `
-            
-        } 
-        
-        if (count == 0) {
-            prevBtn.style.visibility = 'hidden';
-        } else {
-            prevBtn.style.visibility = 'visible';
         }
+    })
 
-        if (count == arrayCbt.length-1) {
-            nextBtn.style.display = 'none';
-            submitBtn.style.display = 'block';            
-        } else {
-            nextBtn.style.display = 'block';
-            submitBtn.style.display = 'none';            
-        }
+    for (let index = 0; index < arrayCbt[count].options.length; index++) {
+        showOptions.innerHTML += `
+        <h5>
+        <input onclick="getAnswer()" name="option" id="my-options" type="radio" value="${arrayCbt[count].options[index]}">
+        <label for="option">${arrayCbt[count].options[index]}</label>
+        </h5>
+        `
+    }
+
+
+    if (count == 0) {
+        prevBtn.style.visibility = 'hidden';
+    } else {
+        prevBtn.style.visibility = 'visible';
+    }
+
+    if (count == arrayCbt.length - 1) {
+        nextBtn.style.display = 'none';
+        submitBtn.style.display = 'block';
+    } else {
+        nextBtn.style.display = 'block';
+        submitBtn.style.display = 'none';
+    }
 
 
 }
 
+let selectedOption = '';
+function getAnswer() {
+    selectedOption = document.querySelector('input[name = option]:checked').value;
+    // console.log(selectedOption);
+    selectedOption.checked;
+}
+
+let choice = [];
+
+
+
+
+
 SortQuestions();
 
-function nextQuestion() {
+function nextQuestion(event) {
+    event.preventDefault();
     showOptions.innerHTML = "";
+    if (choice[count] === "") {
+        choice.push(selectedOption); 
+        quizResult();       
+    } else {
+        choice.splice(count,1,selectedOption);
+        quizResult();       
+    }
+    console.log(choice);
     count++;
     SortQuestions();
+
 }
 
 function previousQuestion() {
     showOptions.innerHTML = "";
     count--;
     SortQuestions();
+}
+
+function submitQuestion() {
+    prevBtn.style.visibility = "hidden";
+    if (choice[count] === "") {
+        choice.push(selectedOption); 
+        quizResult();       
+    } else {
+        choice.splice(count,1,selectedOption);
+        quizResult();       
+    }
+    console.log(choice);
+    console.log("You Score " + score + " out of " + arrayCbt.length);
+    
+}
+
+let score = 0;
+function quizResult() {
+    if (choice[count] === arrayCbt[count].answers) {
+        score = score + 1;
+    } else {
+        score = score + 0;
+    }
+    // console.log("You Score " + score + " out of " + arrayCbt.length);
 }
 
 
